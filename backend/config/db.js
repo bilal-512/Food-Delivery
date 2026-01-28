4721f15e774dd327b4cded594e62fa05d8ed610d
Bilal <danianbilal512@gmail.com>
2026-01-29 03:01:02 +0500

    upto date

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+console.log("MONGO_URL:", process.env.MONGO_URL);
 
 export const connectDB = async () => {
   await mongoose
     .connect(
+      
       process.env.MONGO_URL
     )
     .then(() =>console.log("DB Connected"));
